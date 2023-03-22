662f41a5e7fb5ba18e43a21969cb3a9cc541bc16
bbellsz <93268338+bbellsz@users.noreply.github.com>
2023-03-22 15:45:26 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ def read_name(name: str):
     return {"hello": name}
 
 @app.post("/callname/")
-async def readpost_name(name: str):
+def write_name(name: str):
     return {"hello": name}
 
 handler = Mangum(app)
