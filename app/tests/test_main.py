7b186f01d870fd23a7e73eb0a6d9e3657b9742ad
bbellsz <93268338+bbellsz@users.noreply.github.com>
2023-03-22 15:44:50 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -9,3 +9,9 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
+    
+def test_write_main():
+    name = "Sirada"
+    response = client.get("/callname",json={"name":name})
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
